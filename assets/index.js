edbc611dfffaf8b99d14ff42f96d34a487d1dbe0
Shamsul Islam Sujon <sujon2762@gmail.com>
2021-03-11 15:28:23 +0600

    Initial commit

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+// Put your application javascript here
+
+// // Mobile menu trigger
+// $(".navbar-toggler").on("click", function () {
+//     $(this).toggleClass("open");
+//     // $("#navbar-content").toggleClass("show");
+// });
+
+// let navToggler = document.querySelector('.navbar-toggler');
+
+// navToggler.addEventListener('click', event => {
+//     if (navToggler.classList.contains('open')) {
+//         navToggler.classList.remove('open');
+//     }
+//     else {
+//         navToggler.classList.add('open');
+//     }
+// });
